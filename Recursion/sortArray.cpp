54d6136d81418598219e0809c66c1748db209019
soumyasn <nagabandisoumyasree24047@gmail.com>
2023-09-03 14:24:20 +0530

    Sort array using Recursion

---
+++ Recursion/sortArray.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+void insert(vector<int>&v, int temp){
+    if(v.size()==0||v[v.size()-1]<=temp){
+        v.push_back(temp);
+        return;
+    }
+    int val = v[v.size()-1];
+    v.pop_back();
+    insert(v,temp);
+    v.push_back(val);
+}
+void sort( vector<int>&v){
+    if(v.size()==1){
+        return;
+    }
+    int temp= v[v.size()-1];
+    v.pop_back();
+    sort(v);
+    insert(v,temp);
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int> v;
+    for(int i=0;i<n;i++){
+        int a;
+        cin>>a;
+        v.push_back(a);
+    }
+    sort(v);
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+}
