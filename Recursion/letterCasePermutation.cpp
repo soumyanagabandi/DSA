5ef8d046cd396300abe9efe103d61688aacf40e4
soumyasn <nagabandisoumyasree24047@gmail.com>
2023-09-06 00:37:10 +0530

    letter case permuatation usng recursion

---
+++ Recursion/letterCasePermutation.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve( string ip, string op){
+    if(ip.length() == 0){
+    cout<<op<<endl;
+       return; 
+    }
+    if(isalpha(ip[0])){
+        string op1 = op;
+        string op2 = op;
+        op1.push_back(tolower(ip[0]));
+        op2.push_back(toupper(ip[0]));
+        ip.erase(ip.begin()+0);
+        solve(ip,op1);
+        solve(ip,op2);
+    }else{
+        string op1 = op;
+        op1.push_back(ip[0]);
+        ip.erase(ip.begin()+0);
+        solve(ip,op1);
+    }
+}
+int main(){
+    string ip,op;
+    cin>>ip;
+
+    op = "";
+
+    solve(ip,op);
+    
+}
